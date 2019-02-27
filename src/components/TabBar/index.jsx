bcd2b64d67043094d27a5af94161e9871a157073
czx <chenzhixiang@yit.com>
2019-02-27 17:44:39 +0800

    tabbar

--- src/components/TabBar/index.jsx
+++ src/components/TabBar/index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
+import style from './tabbar.module.scss';
 
 class TabBar extends Component {
   render() {
     return (
-      <div>
-        TabBar
-        <p className="ignore-box">
-          <span>11</span>
-        </p>
+      <div className={style.content}>
+        <NavLink to="/home" activeClassName={style.active}>
+          <span className="iconfont iconbao"></span>
+          <span>书城</span>
+        </NavLink>
+        <NavLink to="/community" activeClassName={style.active}>
+          <span className="iconfont iconshequ"></span>
+          <span>社区</span>
+        </NavLink>
+        <NavLink to="/find" activeClassName={style.active}>
+          <span className="iconfont iconfaxian"></span>
+          <span>发现</span>
+        </NavLink>
       </div>
     );
   }
--- src/route/index.js
+++ src/route/index.js
@@ -1,22 +1,25 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Loadable from './Loadable';
-const Home = Loadable(() => import(/* webpackChunkName:"home" */'@views/home'));
-const Community = Loadable(() => import(/* webpackChunkName:"community" */'@views/community'));
-const Find = Loadable(() => import(/* webpackChunkName:"find" */'@views/find'));
+import React, { Fragment } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Loadable from "./Loadable";
+import TabBar from '@components/TabBar';
+
+const Home = Loadable(() => import(/* webpackChunkName:"home" */ "@views/home"));
+const Community = Loadable(() =>import(/* webpackChunkName:"community" */ "@views/community"));
+const Find = Loadable(() =>import(/* webpackChunkName:"find" */ "@views/find"));
 
 const Routes = () => (
-    <BrowserRouter>
-        <Fragment>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route path='/home' component={Home} />
-                <Route path='/community' component={Community} />
-                <Route path='/find' component={Find} />
-                <Redirect to='/' />
-            </Switch>
-        </Fragment>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/community" component={Community} />
+        <Route path="/find" component={Find} />
+        <Redirect to="/" />
+      </Switch>
+      <TabBar/>
+    </Fragment>
+  </BrowserRouter>
+);
 
 export default Routes;
