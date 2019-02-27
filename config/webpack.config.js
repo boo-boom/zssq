9efd3aa6c192145e398228de24698f4d65c4ab06
czx <chenzhixiang@yit.com>
2019-02-27 15:21:34 +0800

    修改rem配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -100,8 +100,9 @@ module.exports = function(webpackEnv) {
             require('postcss-pxtorem')({
               rootValue: 75,                          // 根像素
               minPixelValue: 2,                       // 最小需要转换的像素
+              propList: ['*'],                        // 需要换算的属性
               unitPrecision: 5,                       // rem单位取小数点后几位
-              selectorBlackList: ['ignoreToRem'],     // 要忽略转换成rem的class
+              selectorBlackList: ['ignore-'],         // 要忽略转换成rem的class前添加ignore-
             })
           ],
           sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
@@ -314,7 +315,7 @@ module.exports = function(webpackEnv) {
               options: {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -347,7 +348,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -386,7 +387,7 @@ module.exports = function(webpackEnv) {
                 ],
                 cacheDirectory: true,
                 cacheCompression: isEnvProduction,
-                
+
                 // If an error happens in a package, it's possible to be
                 // because it was compiled. Thus, we don't want the browser
                 // debugger to show the original code. Instead, the code
